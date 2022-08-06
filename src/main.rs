5fed2a065ffd4f809bc1a5a3a22f66df0da1c65e
idwer <idwer@localhost>
2022-08-06 20:47:18 +0200

    add main() function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use std::env;
+
 fn ape_index(h: u32, w: u32) -> f32 {
     w as f32 / h as f32
+}
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+
+    let height: u32 = args[1].parse::<u32>().unwrap();
+    let wingspan: u32 = args[2].parse::<u32>().unwrap();
+
+    if height < 1 || wingspan < 1 {
+        panic!("height or wingspan found to be unreasonably short (debug: height {}, wingspan {})", height, wingspan);
+    }
+
+    println!("ape index: {}", ape_index(height, wingspan));
 }
 
 #[cfg(test)]
