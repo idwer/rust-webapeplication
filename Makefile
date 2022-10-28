aad465a57005795b8e3b59e4dc2b3ff4c09138dc
User Idwer <foo@bar>
2022-10-28 17:37:32 +0200

    add feature and dependency to log HTTP access to the foreground/stdout

--- Makefile
+++ Makefile
@@ -6,5 +6,11 @@ build:
 check:
 	cargo check
 
+run_log:
+	RUST_LOG=actix_web=info cargo run
+
+run_log_release:
+	RUST_LOG=actix_web=info cargo run -r
+
 test:
 	cargo test
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::App;
 use actix_web::HttpServer;
+use actix_web::middleware::Logger;
 
 mod lib;
 mod routes;
@@ -9,8 +10,11 @@ use crate::routes::ape_post::ape_to_json_post;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+     env_logger::init();
+
     HttpServer::new(|| {
         App::new()
+        .wrap(Logger::default())
         .service(ape_get_show_usage)
         .service(ape_to_json_post)
     })
